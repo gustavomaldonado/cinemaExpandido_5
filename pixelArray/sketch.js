0e46e404af0e1712ab888cabc90075c573859c33
gustavomaldonado <gustavormaldonado@gmail.com>
2018-03-20 18:41:12 -0600

    nuevo array de pixeles

--- pixelArray/sketch.js
+++ pixelArray/sketch.js
@@ -1,7 +1,7 @@
 /*
  *
  * Cinema Expandido Web
- * PixelArray 2/13/2018
+ * Pixel Array (13/feb/18)
  * Gustavo Maldonado
  * 
  *
@@ -16,34 +16,37 @@
  *****************************************
  *****************************************
  */
- 
- var video;
- 
- /*
+
+var video;
+
+/*
  *****************************************
  *****************************************
  * LIFE CYCLE METHODS
  *****************************************
  *****************************************
  */
- 
- function windowsResize(){
-   resizeCanvas(windowWidth,windowHeight);
-   
- }
- 
-function setup() {
-  createCanvas(displayWidth,displayHeight);
-  initilizeVideo();
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
 }
+
+function preload(){
+   video= createVideo("assets/videos/pendulum.MOV");
+}
+
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+  initializeVideo();
+}
+
 
 function draw() {
-  background(255);
-  drawVideo();
-  rect(51,51,51,51);
+  background(0);
+  drawVideo4();
+  toggleVideo();
 }
 
- /*
+/*
  *****************************************
  *****************************************
  * VIDEO METHODS
@@ -51,33 +54,53 @@ function draw() {
  *****************************************
  */
  
- function initilizeVideo(){
-  video = createVideo("assets/videos/pendulum.MOV");
-  video.loop();
-  video.hide();
+ function initializeVideo(){
+  
+   video.loop();
+   video.hide();
  }
  
- function drawVideo(){
-   var correctionX = (windowWidth/2)- video.width/2;
-   var correctionY = (windowHeight/2)- video.height/2;
+ 
+ 
+ function drawVideo4(){ 
+   var correctionX = (windowWidth/2 - video.width/2);
+   var correctionY = (windowHeight/2 - video.height/2);
    
    video.loadPixels();
    
-   for(var y = 0; y < video.height; y++){
-     for(var x = 0; x < video.width; x++){
-       var index = (x + (y * video.width)) * 4;
-       
-       video.pixels[index] = 112; //aqui tengo los rojos
-       video.pixels[index + 1] = video.pixels[index + 1]; //aqui tengo los verdes
-       video.pixels[index + 2] = video.pixels[index + 2]; //aqui tengo los azul
-       video.pixels[index + 3] = video.pixels[index + 3]; //aqui tengo el alpha
-       
-       
-     }
+   var stepSize= round(map(mouseX, 0, windowWidth, 8,60));
+   
+   for(var y=0; y<video.height; y+=stepSize){
+    for(var x=0; x<video.width;x+= stepSize){
+      
+      var index = (x+(y*video.width))*4;
+      
+      var darkness= (255-video.pixels[index])/255;
+      
+      var radio = stepSize * darkness;
+      
+      fill(video.pixels[index],video.pixels[index+1],video.pixels[index+2]);
+      
+      ellipse(x+correctionX,y+correctionY,radio,radio);
+      
+    }
    }
    
-   video.updatePixels();
    
-   image(video,correctionX,correctionY);
+   //video.updatePixels();
    
+   //image(video,correctionX,correctionY);
  }
+ 
+ function toggleVideo(){
+    if(mouseY <= windowHeight/2){
+      video.play();
+    }
+    else{
+      video.pause();
+    }
+}
+    
+
+ 
+ 
